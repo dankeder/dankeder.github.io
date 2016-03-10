2d6025a4e1817493d542ea46c24008eb9d916a57
Dan Keder <dan.keder@gmail.com>
2016-03-10 11:19:46 +0100

    add contact-form js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -10,6 +10,76 @@ $(function() {
 });
 
 
+// Contact form scripts
+(function() {
+  jQuery(document).ready(function($) {
+
+    $('#contact-form #name').change(function(e) {
+      if ($(this).val() == '') {
+        $('#contact-form #name-form-group').addClass('has-error');
+        $('#contact-form #missing-name-alert').removeClass('hide');
+      } else {
+        $('#contact-form #name-form-group').removeClass('has-error');
+        $('#contact-form #missing-name-alert').addClass('hide');
+      }
+    });
+
+    $('#contact-form #email').change(function(e) {
+      if ($(this).val() == '') {
+        $('#contact-form #email-form-group').addClass('has-error');
+        $('#contact-form #missing-email-alert').removeClass('hide');
+      } else {
+        $('#contact-form #email-form-group').removeClass('has-error');
+        $('#contact-form #missing-email-alert').addClass('hide');
+      }
+    });
+
+    $('#contact-form #text').change(function(e) {
+      if ($(this).val() == '') {
+        $('#contact-form #text-form-group').addClass('has-error');
+        $('#contact-form #missing-text-alert').removeClass('hide');
+      } else {
+        $('#contact-form #text-form-group').removeClass('has-error');
+        $('#contact-form #missing-text-alert').addClass('hide');
+      }
+    });
+
+    $('#contact-form').submit(function(e) {
+      e.preventDefault();
+      var data = {
+        name: $('#contact-form #name').val(),
+        email: $('#contact-form #email').val(),
+        text: $('#contact-form #text').val(),
+      };
+
+      // form validation
+      if ((data.name == '') || (data.email == '') || (data.text == '')) {
+        return;
+      }
+
+      var posting = $.ajax({
+        type: 'POST',
+        url: "https://develcraft.com/api/send-mail",
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data)
+      });
+      posting
+      .done(function (data) {
+        if (data.ok) {
+          $('#send-success-alert').removeClass('hide');
+          $('#contact-form-wrapper').addClass('hide');
+        } else {
+          $('#send-failed-alert').removeClass('hide');
+        }
+      })
+      .fail(function() {
+        $('#send-failed-alert').removeClass('hide');
+      });
+    });
+
+  });
+})();
+
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
     var MQL = 1170;
